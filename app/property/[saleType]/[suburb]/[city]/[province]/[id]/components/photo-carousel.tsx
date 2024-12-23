f5fc8b9cd6da88ae144f3555cbd79be1ff2aedb8
Ian <vanzyli101@gmail.com>
2024-12-23 11:24:46 +0200

    Commit

--- app/property/[saleType]/[suburb]/[city]/[province]/[id]/components/photo-carousel.tsx
+++ app/property/[saleType]/[suburb]/[city]/[province]/[id]/components/photo-carousel.tsx
@@ -9,6 +9,7 @@ import 'swiper/css';
 import '../css/swiper/navigation.css';
 import '../css/swiper/pagination.css';
 import '../css/swiper/swiper-container.css';
+
 import {useState} from "react";
 import {cn} from "@/lib/utils";
 
@@ -52,3 +53,37 @@ export default function PhotoCarousel({photoUrls}: PhotoCarouselProps) {
         </div>
     );
 };
+
+
+
+// 'use client'
+// import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+//
+// import { Swiper, SwiperSlide } from 'swiper/react';
+//
+// // Import Swiper styles
+// import 'swiper/css';
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
+// import 'swiper/css/scrollbar';
+//
+// export default function PhotoCarousel() {
+//     return (
+//         <Swiper
+//             // install Swiper modules
+//             modules={[Navigation, Pagination, A11y]}
+//             spaceBetween={50}
+//             slidesPerView={1}
+//             navigation
+//             pagination={{ clickable: true }}
+//             scrollbar={{ draggable: true }}
+//             onSwiper={(swiper) => console.log(swiper)}
+//             onSlideChange={() => console.log('slide change')}
+//         >
+//             <SwiperSlide>Slide 1</SwiperSlide>
+//             <SwiperSlide>Slide 2</SwiperSlide>
+//             <SwiperSlide>Slide 3</SwiperSlide>
+//             <SwiperSlide>Slide 4</SwiperSlide>
+//         </Swiper>
+//     );
+// };
